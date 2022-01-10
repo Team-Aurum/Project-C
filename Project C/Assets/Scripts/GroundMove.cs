5c8e71a98e3be12a6c5fc4915d284db745e11e7e
Pineapple42 <frozenfire71@gmail.com>
2022-01-10 13:40:47 -0600

    Movement change
    Forward and Backwards are now set
    
    In the future, backwards moving functionality will be completely removed as forwards movement is only used for entering dungeons/sub-areas in The Expanse

--- Project C/Assets/Scripts/GroundMove.cs
+++ Project C/Assets/Scripts/GroundMove.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine; public class GroundMove : MonoBehaviour
 {
+    IEnumerator coroutine;
+    bool groundCoroutineRunning = false;
     int speed = 30;
     // Start is called before the first frame update
     void Start()
@@ -12,16 +14,33 @@ using UnityEngine; public class GroundMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.S))
-            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.S) && !groundCoroutineRunning){
+            coroutine = MoveUpDown(1);
+            groundCoroutineRunning = true;
+            StartCoroutine(coroutine);
+        }
+            // transform.Rotate(Vector3.up * speed * Time.deltaTime);
       
-        if (Input.GetKey(KeyCode.W))
-            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.W) && !groundCoroutineRunning){
+            coroutine = MoveUpDown(-1);
+            groundCoroutineRunning = true;
+            StartCoroutine(coroutine);
+        }
 
         if (Input.GetKey(KeyCode.A) && transform.position.x < 140 )
             transform.Translate(Vector3.up * speed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.D) && transform.position.x > -140)
             transform.Translate(-Vector3.up * speed * Time.deltaTime);
+    }
+
+    IEnumerator MoveUpDown(int up){
+        int count = 0;
+        while(count < 50){
+            transform.Rotate(up * Vector3.up * speed * Time.deltaTime);
+            count++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        groundCoroutineRunning = false;
     }
 }
--- Project C/Assets/Scripts/ObjectMove.cs
+++ Project C/Assets/Scripts/ObjectMove.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class ObjectMove : MonoBehaviour
 {
+    IEnumerator objectCoroutine;
+    bool objectCoroutineRunning = false;
     int speed = 30;
     float deltaRotation;
     public Transform cylinder;
@@ -18,50 +20,67 @@ public class ObjectMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && !objectCoroutineRunning){
+            objectCoroutine = ObjectUpDown(1);
+            objectCoroutineRunning = true;
+            StartCoroutine(objectCoroutine);
             //transform.Translate(Vector3.down * (speed) * Time.deltaTime);
-            if (transform.position.z > 8)
-            {
-                transform.Translate(new Vector3(0, (float)-0.4, 1) * (speed) * Time.deltaTime);
-            }
-            else if (transform.position.z < -8 && transform.position.z > -14)
-            {
-                transform.Translate(new Vector3(0, (float)0.4, 1) * (speed) * Time.deltaTime);
-            }
-            else if (transform.position.z < -14) 
-            {
-                transform.Translate(new Vector3(0, (float)0.45, 1) * (speed) * Time.deltaTime);
-            }
-            else
-            {
-                transform.SetPositionAndRotation(new Vector3(transform.position.x, 50, transform.position.z), transform.rotation);
-                transform.Translate(new Vector3(0, 0, 1) * (speed) * Time.deltaTime);
-            }
+        }
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && !objectCoroutineRunning){
+            objectCoroutine = ObjectUpDown(-1);
+            objectCoroutineRunning = true;
+            StartCoroutine(objectCoroutine);
+        }
             //transform.Translate(-Vector3.down * (speed) * Time.deltaTime);
-            if (transform.position.z > 8)
-            {
-                transform.Translate(new Vector3(0, (float)0.4, -1) * (speed) * Time.deltaTime);
-            }
-            else if (transform.position.z < -8 && transform.position.z > -14)
-            {
-                transform.Translate(new Vector3(0, (float)-0.4, -1) * (speed) * Time.deltaTime);
-            }
-            else if (transform.position.z < -14)
-            {
-                transform.Translate(new Vector3(0, (float)-0.45, -1) * (speed) * Time.deltaTime);
-            }
-            else 
-            {
-                transform.SetPositionAndRotation(new Vector3(transform.position.x, 50, transform.position.z), transform.rotation);
-                transform.Translate(new Vector3(0, 0, -1) * (speed) * Time.deltaTime);
-            }
+            // if (transform.position.z > 8)
+            // {
+            //     transform.Translate(new Vector3(0, (float)0.4, -1) * (speed) * Time.deltaTime);
+            // }
+            // else if (transform.position.z < -8 && transform.position.z > -14)
+            // {
+            //     transform.Translate(new Vector3(0, (float)-0.4, -1) * (speed) * Time.deltaTime);
+            // }
+            // else if (transform.position.z < -14)
+            // {
+            //     transform.Translate(new Vector3(0, (float)-0.45, -1) * (speed) * Time.deltaTime);
+            // }
+            // else 
+            // {
+            //     transform.SetPositionAndRotation(new Vector3(transform.position.x, 50, transform.position.z), transform.rotation);
+            //     transform.Translate(new Vector3(0, 0, -1) * (speed) * Time.deltaTime);
+            // }
 
         if (Input.GetKey(KeyCode.A))
             transform.Translate(Vector3.right * (speed) * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.D))
             transform.Translate(-Vector3.right * (speed) * Time.deltaTime);
+    }
+
+    IEnumerator ObjectUpDown(int up){
+        int count = 0;
+        while(count < 50){
+            if (transform.position.z > 8)
+            {
+                transform.Translate(new Vector3(0, (float)-0.4*up, up) * (speed) * Time.deltaTime);
+            }
+            else if (transform.position.z < -8 && transform.position.z > -14)
+            {
+                transform.Translate(new Vector3(0, (float)0.4*up, up) * (speed) * Time.deltaTime);
+            }
+            else if (transform.position.z < -14) 
+            {
+                transform.Translate(new Vector3(0, (float)0.45*up, up) * (speed) * Time.deltaTime);
+            }
+            else
+            {
+                transform.SetPositionAndRotation(new Vector3(transform.position.x, 50, transform.position.z), transform.rotation);
+                transform.Translate(new Vector3(0, 0, 1*up) * (speed) * Time.deltaTime);
+            }
+            count++;
+            yield return new WaitForSeconds(0.01f);
+        }
+         objectCoroutineRunning = false;
     }
 }
