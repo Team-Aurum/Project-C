c7c6a9c599425e2d96d8218619fa6df527e5b038
Vincent Chen <32251990+Jordan-Limonov@users.noreply.github.com>
2023-05-26 23:24:01 -0500

    Update Tech.gd
    Casual placeholder "modify()" function for when the techs get jiggy with it

--- Project C/Scripts/Battle/Generics/Tech.gd
+++ Project C/Scripts/Battle/Generics/Tech.gd
@@ -25,6 +25,9 @@ func _init():
 func getColors():
 	return [Color("#FF00FF"), Color("#FF00FF")];
 
+func modify():
+	pass;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
